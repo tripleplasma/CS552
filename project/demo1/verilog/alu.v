48878de7f00c2518c7ed5db8a443c7388cba5017
Allie <alliebacholl@gmail.com>
2024-10-14 01:56:43 -0500

    Adds 'all' functionality to ALU, needs testing and opcodes

--- project/demo1/verilog/alu.v
+++ project/demo1/verilog/alu.v
@@ -9,53 +9,72 @@
     of the operation, as well as drive the output signals Zero and Overflow
     (OFL).
 */
-module alu (InA, InB, Cin, Oper, invA, invB, sign, Out, Zero, Ofl);
+module alu (InA, InB, Cin, Oper, sign, Out, zf, sf, of, cf);
 
     parameter OPERAND_WIDTH = 16;    
-    parameter NUM_OPERATIONS = 3;
+    parameter NUM_OPERATIONS = 4;
        
     input  [OPERAND_WIDTH-1:0]  InA ; // Input operand A
     input  [OPERAND_WIDTH-1:0]  InB ; // Input operand B
     input                       Cin ; // Carry in
     input  [NUM_OPERATIONS-1:0] Oper; // Operation type
-    input                       invA; // Signal to invert A
-    input                       invB; // Signal to invert B
     input                       sign; // Signal for signed operation
     output [OPERAND_WIDTH-1:0]  Out ; // Result of computation
-    output                      Ofl ; // Signal if overflow occured
-    output                      Zero; // Signal if Out is 0
+    output                      of  ; // Signal if overflow occured
+    output                      sf  ; // Signal if Out is negative or positive
+    output                      zf  ; // Signal if Out is 0
+    output                      cf  ; // Signal if carry out is 1
 
     /* YOUR CODE HERE */
     // Intermediate signals
     wire [OPERAND_WIDTH-1:0] A_int, B_int;
-    wire [OPERAND_WIDTH-1:0] shift_result, sum, and_result, or_result, xor_result;
-    wire                     carry_out;
+    wire [OPERAND_WIDTH-1:0] shift_result, sum, xor_result, andn_result;
+    wire [3:0]               ShAmt;
     
-    // Invert A and B if specified (for substraction or rotate/shift)
-    assign A_int = (invA) ? ~InA : InA;
-    assign B_int = (invB) ? ~InB : InB;
+    // Invert A and B if specified (for subtraction and ANDN)
+    assign A_int = (Oper == 3'b0101) ? ~InA : InA;
+    assign B_int = (Oper == 3'b0111 || Oper == 3'b10XX) ? ~InB : InB;
     
-    // Barrel shifter 
-    shifter iSHIFTER(.In(A_int), .ShAmt(B_int[3:0]), .Oper(Oper[1:0]), .Out(shift_result));
+    // Barrel shifter, shift amount is 8 for SLBI, 4 lower bits of B otherwise
+    assign ShAmt = (Oper == 4'b1100) ? 4'b1000 : B_int[3:0];
+    shifter iSHIFTER(.InA(A_int), .ShAmt(ShAmt), .Oper(Oper[1:0]), .Out(shift_result));
     
     // Arithmetic addition
-    cla_16b iCLA_16b(.sum(sum), .c_out(carry_out),.a(A_int), .b(B_int), .c_in(Cin));
+    cla_16b iCLA_16b(.sum(sum), .c_out(cf),.a(A_int), .b(B_int), .c_in(Cin));
     
-    // Bitwise AND, OR, XOR
-    assign and_result = A_int & B_int;
-    assign or_result  = A_int | B_int;
-    assign xor_result = A_int ^ B_int;
+    // Bitwise XOR and ANDN
+    assign xor_result  = A_int ^ B_int;
+    assign andn_result = A_int & B_int;
     
     // Overflow detection
-    overflow iOVERFLOW(.A(A_int), .B(B_int), .sum(sum), .carry_out(carry_out), .Oper(Oper), .sign(sign), .Ofl(Ofl));
+    overflow iOVERFLOW(.A(A_int), .B(B_int), .sum(sum), .carry_out(cf), .Oper(Oper), .sign(sign), .of(Ofl));
     
     // Zero flag
-    assign Zero = (Out == 16'b0) ? 1'b1 : 1'b0;
+    assign zf = (Out == 16'b0) ? 1'b1 : 1'b0;
+
+    // Sign flag, equal to MSB of output
+    assign sf = Out[OPERAND_WIDTH-1];
+
+    // Carry flag
+    assign cf = carry_out;
+
+    // Assign bit reversal
+    wire brt;
+    assign brt = {InA[0], InA[1], InA[2], InA[3], InA[4], InA[5], InA[6], InA[7], InA[8], InA[9], InA[10], InA[11], InA[12], InA[13], InA[14], InA[15]};
 
     // Output mux to select the correct operation result
-    assign Out = (Oper[2] == 0)   ? shift_result :  // use barrel shifter
-                 (Oper == 3'b100) ? sum :           // A + B
-                 (Oper == 3'b101) ? and_result :    // A AND B
-                 (Oper == 3'b110) ? or_result :     // A OR B
-                 xor_result;                        // A XOR B
+    assign Out =    (Oper[3] == 0)          ?                           // Check if make use of the functions
+                    (Oper[2] == 0)          ? shift_result :            // use barrel shifter
+                    (Oper[2:0] == 3'b100)   ? sum :                     // A + B
+                    (Oper[2:0] == 3'b101)   ? sum :                     // B - A
+                    (Oper[2:0] == 3'b110)   ? xor_result :              // A XOR B
+                    andn_result             :                           // A AND ~B, else Oper[3] == 1
+                    (Oper[2:0] == 3'b000)   ? zf :                      // Set if A = B
+                    (Oper[2:0] == 3'b001)   ? sf :                      // Set if A < B
+                    (Oper[2:0] == 3'b010)   ? (zf | sf) :               // Set if A <= B
+                    (Oper[2:0] == 3'b100)   ? cf :                      // Set if A + B generates a carry out
+                    (Oper[2:0] == 3'b101)   ? InB :                     // LBI: Out = InB
+                    (Oper[2:0] == 3'b110)   ? brt :                     // Reverse the bits
+                    (Oper[2:0] == 3'b111)   ? (shift_result | InB) :    // SLBI: Rs<<8 | I(zero ext.)
+                    ;
 endmodule
--- project/demo1/verilog/execute.v
+++ project/demo1/verilog/execute.v
@@ -5,9 +5,48 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (/* TODO: Add appropriate inputs/outputs for your execute stage here*/);
+module execute (read1Data, read2Data, opcode, extention, aluSrc, immExt, aluOut, zf, sf, of, cf);
 
-   // TODO: Your code here
+   input [15:0]   read1Data ;    // Input operand A
+   input [15:0]   read2Data ;    // Output of the 2nd read of the register file
+   input [4:0]    opcode;        // Top 5 bits of instruction
+   input [1:0]    extention;     // Bottom 2 bits for R-format instructions
+   input          aluSrc;        // Indicates if inB to ALU should be the output of the register file
+                                 // or the output of the extention module
+   input [15:0]   immExt ;       // Output of the immediate extention module
+
+   
+   output   [15:0]   aluOut;  // ALU output value
+   output   sf; // Signal if Out is negative or positive
+   output   zf; // Signal if Out is 0
+   output   of; // Signal if overflow occured
+   output   cf; // Signal if carry out is 1
+
+   // Combination of opcode and the the opcode extention
+   // to for the opcode fed into the ALU
+   wire [7:0] aluOp;
+   assign aluOp = {opcode, extention};
+
+   // Assign InB based on aluSrc value
+   wire  [15:0] InB;
+   assign InB = (aluSrc) ? immExt : read2Data;
+
+   // TODO SLBI unsigned
+
+   // Create the ALU
+   alu_hier #(.NUM_OPERATIONS(4)) 
+      iALU(// Inputs
+         .InA(read1Data), 
+         .InB(inB), 
+         .Cin(0), 
+         .Oper(aluOp), 
+         .sign(1),
+         // Outputs 
+         .Out(aluOut), 
+         .zf(zf), 
+         .sf(sf),
+         .of(of),
+         .cf(cf));
    
 endmodule
 `default_nettype wire
