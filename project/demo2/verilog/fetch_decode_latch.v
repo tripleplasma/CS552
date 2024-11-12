49ab5ebac6d234f683494236b2b292aecdca0127
Allie Bacholl <bacholl@snares-10.cs.wisc.edu>
2024-11-12 12:59:32 -0600

    Branch runs for 2 cycles

--- project/demo2/verilog/fetch_decode_latch.v
+++ project/demo2/verilog/fetch_decode_latch.v
@@ -11,12 +11,12 @@ module fetch_decode_latch(clk, rst, nop, nop_ctrl, rst_d, PC_f, PC_d, instructio
 
     wire latch_nop_ctrl_1, latch_nop_ctrl_2;
     register #(.REGISTER_WIDTH(1)) CtrlLatch1(.clk(clk), .rst(1'b0), .writeEn(1'b1), .writeData(nop_ctrl), .readData(latch_nop_ctrl_1));
-    register #(.REGISTER_WIDTH(1)) CtrlLatch2(.clk(clk), .rst(1'b0), .writeEn(1'b1), .writeData(latch_nop_ctrl_1), .readData(latch_nop_ctrl_2));
+    //register #(.REGISTER_WIDTH(1)) CtrlLatch2(.clk(clk), .rst(1'b0), .writeEn(1'b1), .writeData(latch_nop_ctrl_1), .readData(latch_nop_ctrl_2));
 
     register #(.REGISTER_WIDTH(1)) iRST_LATCH_FD(.clk(clk), .rst(1'b0), .writeEn(1'b1), .writeData(rst), .readData(rst_d));
     register iPC_LATCH_FD(.clk(clk), .rst(rst), .writeEn(~nop), .writeData(PC_f), .readData(PC_d));
     //assign PC_d = (nop) ? PC_d : pc_fd_int;
     register iINSTRUCTION_LATCH_FD(.clk(clk), .rst(rst), .writeEn(1'b1), .writeData(instruction_f), .readData(instruction_fd_int));
-    assign instruction_d = (latch_nop_ctrl_2) ? 16'b0000_1000_0000_0000 : ((nop) ? instruction_d : instruction_fd_int);
+    assign instruction_d = (latch_nop_ctrl_1) ? 16'b0000_1000_0000_0000 : ((nop) ? instruction_d : instruction_fd_int);
 
 endmodule
