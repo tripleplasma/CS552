206bf45c4490e5111ea0b84119c7f3dfe09bbfcb
tripleplasma <kalee9@wisc.edu>
2024-11-14 19:54:18 -0600

    Fixed NOP thinking BR are using R0 and throwing data hazards

--- project/demo2/verilog/hdu.v
+++ project/demo2/verilog/hdu.v
@@ -13,11 +13,22 @@ module hdu (clk, rst,
     output wire disablePCWrite, disableIFIDWrite, setExNOP, setFetchNOP;
 
 
-    //TODO: Check if the opcode_f is a valid R type instruction so that we don't confuse immediate bits for register names
+
+
+    //TODO: Check if the opcodes are a valid R type instruction so that we don't confuse immediate bits for register names
+    //                                 JMP                    JAL           NOP
+    wire ignoreReg1 = (opcode_d == 5'b00100 | opcode_d == 5'b00110 | opcode_d == 5'b00001);
+
+    //                                    JMP                       BR                    LBI                    SLBI                   BTR             NOP
+    wire ignoreReg2 = (opcode_d[4:2] == 3'b001 | opcode_d[4:2] == 3'b011 | opcode_d == 5'b11000 | opcode_d == 5'b10010 | opcode_d == 5'b11001 | opcode_d == 5'b00001);
+
+    wire RAW_ID_EX = (((idExWriteRegister == ifIdReadRegister1) & ~ignoreReg1) | ((idExWriteRegister == ifIdReadRegister2) & ~ignoreReg2)) & |PC_e;
+    wire RAW_EX_MEM = (((exMemWriteRegister == ifIdReadRegister1) & ~ignoreReg1) | ((exMemWriteRegister == ifIdReadRegister2) & ~ignoreReg2)) & |PC_m;
+    wire RAW_MEM_WB = (((memWbWriteRegister == ifIdReadRegister1) & ~ignoreReg1) | ((memWbWriteRegister == ifIdReadRegister2) & ~ignoreReg2)) & |PC_wb;
     //TODO: make a check to make sure that the instructions at those stages aren't NOPs otherwise it'll think R0 is being used
-    wire data_hazard = (rst == 1'b0) & ((idExWriteRegister == ifIdReadRegister1  | idExWriteRegister == ifIdReadRegister2 & |PC_e)      |
-                                        (exMemWriteRegister == ifIdReadRegister1 | exMemWriteRegister == ifIdReadRegister2 & |PC_m)     |
-                                        (memWbWriteRegister == ifIdReadRegister1 | memWbWriteRegister == ifIdReadRegister2 & |PC_wb));
+    wire RAW_hazard = RAW_ID_EX | RAW_EX_MEM | RAW_MEM_WB;
+
+    wire data_hazard = (rst == 1'b0) & RAW_hazard;
 
 
     wire control_hazard =   (opcode_f[4:2] == 3'b001 | opcode_f[4:2] == 3'b011) | 
@@ -25,6 +36,7 @@ module hdu (clk, rst,
                             (opcode_e[4:2] == 3'b001 | opcode_e[4:2] == 3'b011) | 
                             (opcode_m[4:2] == 3'b001 | opcode_m[4:2] == 3'b011) ;
 
+    //NOTE: We're disabling the PCWrite when the HALT is read because otherwise we'll get XXXX's as the instruction and it will break everything, thats whay the opcode_f== is for
     assign disablePCWrite = data_hazard | control_hazard | (opcode_f == 5'b00000);
 
     //NOTE: If we setExNOP, we need to keep the decode instruction at the IFID latch so that when the hazard is gone, the instruction is still there
