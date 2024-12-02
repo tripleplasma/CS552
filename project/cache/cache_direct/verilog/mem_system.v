878d7ca275a19040b232cb5074468afd548b9804
Khiem Vu <khiemvu117@gmail.com>
2024-12-02 14:49:41 -0600

    implementing schematic

--- project/cache/cache_direct/verilog/mem_system.v
+++ project/cache/cache_direct/verilog/mem_system.v
@@ -24,45 +24,72 @@ module mem_system(/*AUTOARG*/
    output reg        CacheHit;
    output reg        err;
 
+   // cache controller signals
+   // cache inputs
+   wire cache_en, force_disable, cache_comp, cache_read, cache_write;
+   wire [15:0] cache_data_in, cache_addr;
+
+   // cache outputs
+   wire cache_hit, cache_valid, cache_dirty;
+   wire real_hit, victimize;
+   wire [4:0] actual_tag;
+   wire [15:0] cache_data_out;
+
+   // 4-bank memory inputs
+   wire mem_write, mem_read;
+   wire [15:0] mem_data_in, mem_addr;
+
+   // 4-bank memory outputs
+   wire mem_stall, mem_busy;
+   wire [15:0] mem_data_out;
+
+   // err signals
+   wire controller_err, cache_err, mem_err;
+   
+   assign cache_en = (cache_read | cache_write) & (~force_disable);
+
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
    parameter memtype = 0;
    cache #(0 + memtype) c0(// Outputs
-                          .tag_out              (),
-                          .data_out             (),
-                          .hit                  (),
-                          .dirty                (),
-                          .valid                (),
-                          .err                  (),
+                          .tag_out              (actual_tag),
+                          .data_out             (cache_data_out),
+                          .hit                  (cache_hit),
+                          .dirty                (cache_dirty),
+                          .valid                (cache_valid),
+                          .err                  (cache_err),
                           // Inputs
-                          .enable               (),
-                          .clk                  (),
-                          .rst                  (),
-                          .createdump           (),
-                          .tag_in               (),
-                          .index                (),
-                          .offset               (),
-                          .data_in              (),
-                          .comp                 (),
-                          .write                (),
-                          .valid_in             ());
+                          .enable               (cache_en),
+                          .clk                  (clk),
+                          .rst                  (rst),
+                          .createdump           (createdump),
+                          .tag_in               (cache_addr[15:11]),
+                          .index                (cache_addr[10:3]),
+                          .offset               (cache_addr[2:0]),
+                          .data_in              (cache_data_in),
+                          .comp                 (cache_comp),
+                          .write                (cache_write),
+                          .valid_in             (1'b1)); // maybe
+
+   assign real_hit = cache_valid & cache_hit;
+   assign victimize = (~hit) & dirty;
 
    four_bank_mem mem(// Outputs
-                     .data_out          (),
-                     .stall             (),
-                     .busy              (),
-                     .err               (),
+                     .data_out          (mem_data_out),
+                     .stall             (mem_stall),
+                     .busy              (mem_busy),
+                     .err               (mem_err),
                      // Inputs
-                     .clk               (),
-                     .rst               (),
-                     .createdump        (),
-                     .addr              (),
-                     .data_in           (),
-                     .wr                (),
-                     .rd                ());
+                     .clk               (clk),
+                     .rst               (rst),
+                     .createdump        (createdump),
+                     .addr              (mem_addr),
+                     .data_in           (mem_data_in),
+                     .wr                (mem_write),
+                     .rd                (mem_read));
    
    // your code here
-
+   assign err = controller_err | cache_err | mem_err;
    
 endmodule // mem_system
 `default_nettype wire
