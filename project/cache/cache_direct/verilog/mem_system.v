1f4707d619c8d91b0d94edb9ce0792dad92819a5
Allie Bacholl <bacholl@vm-instunix-08.cs.wisc.edu>
2024-12-02 22:03:40 -0600

    Compiles

--- project/cache/cache_direct/verilog/mem_system.v
+++ project/cache/cache_direct/verilog/mem_system.v
@@ -26,8 +26,9 @@ module mem_system(/*AUTOARG*/
 
    // cache controller signals
    // cache inputs
-   wire cache_en, force_disable, cache_comp, cache_read, cache_write;
-   wire [15:0] cache_data_in, cache_addr;
+   reg cache_en, cache_comp, cache_read, cache_write;
+   wire force_disable;
+   reg [15:0] cache_data_in, cache_addr;
 
    // cache outputs
    wire cache_hit, cache_valid, cache_dirty;
@@ -36,8 +37,8 @@ module mem_system(/*AUTOARG*/
    wire [15:0] cache_data_out;
 
    // 4-bank memory inputs
-   wire mem_write, mem_read;
-   wire [15:0] mem_data_in, mem_addr;
+   reg mem_write, mem_read;
+   reg [15:0] mem_data_in, mem_addr;
 
    // 4-bank memory outputs
    wire mem_stall;
@@ -49,17 +50,17 @@ module mem_system(/*AUTOARG*/
 
 
    assign force_disable = mem_stall;
-   wire mem_to_cache = ((mem_addr[2:1] == 2'b00 & ~mem_busy[0]) | 
-                        (mem_addr[2:1] == 2'b01 & ~mem_busy[1]) | 
-                        (mem_addr[2:1] == 2'b10 & ~mem_busy[2]) | 
-                        (mem_addr[2:1] == 2'b11 & ~mem_busy[3])) ? 1'b1 : 1'b0;
+   // wire mem_to_cache = ((mem_addr[2:1] == 2'b00 & ~mem_busy[0]) | 
+   //                      (mem_addr[2:1] == 2'b01 & ~mem_busy[1]) | 
+   //                      (mem_addr[2:1] == 2'b10 & ~mem_busy[2]) | 
+   //                      (mem_addr[2:1] == 2'b11 & ~mem_busy[3])) ? 1'b1 : 1'b0;
 
-   assign cache_addr = Addr; // () ? : ; maybe
-   assign cache_data_in = (mem_to_cache) ? mem_data_out : DataIn;
-   assign cache_read = Rd;
-   assign cache_write = mem_to_cache | Wr;
-   assign cache_en = (cache_read | cache_write) & (~force_disable);
-   assign cache_comp = (cache_read | cache_write) & (~victimize) & (~mem_to_cache);
+   // assign cache_addr = Addr; // () ? : ; maybe
+   // assign cache_data_in = (mem_to_cache) ? mem_data_out : DataIn;
+   // assign cache_read = Rd;
+   // assign cache_write = mem_to_cache | Wr;
+   // assign cache_en = (cache_read | cache_write) & (~force_disable);
+   // assign cache_comp = (cache_read | cache_write) & (~victimize) & (~mem_to_cache);
 
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
@@ -84,13 +85,13 @@ module mem_system(/*AUTOARG*/
                           .write                (cache_write),
                           .valid_in             (1'b1)); // maybe
 
-   assign real_hit = cache_valid & cache_hit;
-   assign victimize = (~cache_hit) & cache_dirty;
-
-   assign mem_addr = (victimize) ? {actual_tag, cache_addr[10:0]} : cache_addr;
-   assign mem_data_in = cache_data_out; // (victimize) ? cache_data_out : ;
-   assign mem_write = victimize;
-   assign mem_read = ~real_hit;
+   // assign real_hit = cache_valid & cache_hit;
+   // assign victimize = (~cache_hit) & cache_dirty;
+    
+   // assign mem_addr = (victimize) ? {actual_tag, cache_addr[10:0]} : cache_addr;
+   // assign mem_data_in = cache_data_out; // (victimize) ? cache_data_out : ;
+   // assign mem_write = victimize;
+   // assign mem_read = ~real_hit;
 
    four_bank_mem mem(// Outputs
                      .data_out          (mem_data_out),
@@ -105,15 +106,154 @@ module mem_system(/*AUTOARG*/
                      .data_in           (mem_data_in),
                      .wr                (mem_write),
                      .rd                (mem_read));
+
+   // 000 Comp 001 ACCESS_READ 010 DIRTY 011 CACHE_WRITEBACK 100 MEM_READ
+   // TODO can use localparems for states
+   reg [3:0] cache_state;
+   reg [3:0] nxt_cache_state;
+   // reg done_rdy, stall_rdy;
+
+   /// OUTPUT FLOPS ///
+   // State flop
+   //dff state_ff[3:0](.d(nxt_cache_state), .q(cache_state), .rst(rst), .clk(clk));
+   // Done flop
+   //dff done_ff(.d(done_rdy), .q(Done), .rst(rst), .clk(clk));
+   // Stall flop
+   //dff stall_ff(.d(stall_rdy), .q(Stall), .rst(rst), .clk(clk));
+   // Cache hit flop
+   //dff cacheHit_ff(.d(cache_hit & cache_valid), .q(CacheHit), .rst(rst), .clk(clk));
+   // Data out flop
+   //dff dataOut_ff[15:0](.d(cache_data_out), .q(DataOut), .rst(rst), .clk(clk));
+   // err flop
+   //dff err_ff(.d(mem_err | cache_err), .q(err), .rst(rst), .clk(clk));
+
+   always @(*) begin
+      // Set default values
+      // cache controller signals
+      // cache inputs
+      cache_en = 1'b0;
+      cache_comp = 1'b0;
+      cache_read = Rd;
+      cache_write = Wr;
+      cache_data_in = DataIn;
+      cache_addr = Addr;
+
+      // Top outops
+      Stall = 1'b0;
+      // TODO Done won't be asserted for 1 cycle, may be less
+      Done = 1'b0;
+      CacheHit = cache_hit & cache_valid;
+      DataOut = cache_data_out;
+      err = mem_err | cache_err;
+
+      cache_state = nxt_cache_state;
+   
+      // cache outputs
+      // wire cache_hit, cache_valid, cache_dirty;
+      // wire real_hit, victimize;
+      // wire [4:0] actual_tag;
+      // wire [15:0] cache_data_out;
+   
+      // 4-bank memory inputs
+      mem_write = 1'b0;
+      mem_read = 1'b0;
+      mem_data_in = cache_data_out;
+      mem_addr = cache_addr;
+   
+      // 4-bank memory outputs
+      // wire mem_stall;
+      // wire [3:0] mem_busy;
+      // wire [15:0] mem_data_out;
+
+      // State machine
+      case (cache_state)
+         // Do cache comparison read or write, also IDLE
+         3'b000: begin
+            cache_en = Rd | Wr;
+            cache_comp = Rd | Wr;
+            if (Rd | Wr) begin
+               // Access to see if hit
+               nxt_cache_state = 3'b001;
+            end
+         end
+
+         3'b001: begin
+            // Miss so need to do access read
+            if (~cache_hit | ~cache_valid) begin
+               cache_en = 1'b1;
+               cache_comp = 1'b0;
+               cache_write = 1'b0;
+               cache_read = 1'b1;
+               // Keep cache address the same
+               Stall = 1'b1;
+               cache_addr = cache_addr;
+               nxt_cache_state = 3'b010;
+            end else begin
+               // Hit so done and can take in new input
+               Done = 1'b1;
+               nxt_cache_state = 3'b000;
+            end
+         end
+          
+         3'b010: begin
+            // Keep cache address the same
+            Stall = 1'b1;
+            cache_addr = cache_addr;
+            if (cache_dirty) begin
+               // Dirty so do memory writeback
+               mem_addr = {actual_tag, cache_addr[10:0]};
+               mem_write = 1'b1;
+               nxt_cache_state = 3'b011;
+            end else begin
+               // Not dirty so can do mem read
+               mem_addr = cache_addr;
+               mem_read = 1'b1;
+               nxt_cache_state = 3'b100;
+            end
+
+         end
+
+         3'b011: begin
+            // Keep cache address the same
+            Stall = 1'b1;
+            cache_addr = cache_addr;
+            // Finished writeback when mem no longer busy
+            if (~|mem_busy) begin
+               mem_read = 1'b1;
+               nxt_cache_state = 3'b100;
+            end
+         end
+
+         3'b100: begin
+            // Keep cache address the same
+            Stall = 1'b1;
+            cache_addr = cache_addr;
+            // Finished read when mem no longer busy
+            if (~|mem_busy) begin
+               // Can now do access write
+               cache_en = 1'b1;
+               cache_comp = 1'b0;
+               cache_write = 1'b1;
+               cache_read = 1'b0;
+               cache_data_in = mem_data_out;
+               Done = 1'b1;
+               nxt_cache_state = 3'b000;
+            end
+         end
+
+         default: nxt_cache_state = 3'b000;
+      endcase
+    end
    
    // Module Outputs
-   always @(*) begin
-      DataOut = (real_hit) ? cache_data_out : mem_data_out;
-      Done = real_hit | mem_to_cache;
-      Stall = mem_stall;
-      CacheHit = real_hit;
-      err = cache_err | mem_err; // | controller_err;
-   end
+   //always @(*) begin
+   //   DataOut = (real_hit) ? cache_data_out : mem_data_out;
+   //   Done = real_hit | mem_to_cache;
+   //   Stall = mem_stall;
+   //   CacheHit = real_hit;
+   //   err = cache_err | mem_err; // | controller_err;
+   //end
+
    
 endmodule // mem_system
 `default_nettype wire
