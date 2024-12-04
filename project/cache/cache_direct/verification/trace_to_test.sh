db8f65e847a340b28b0216ab1e94ba9c431b3770
tripleplasma <kalee9@wisc.edu>
2024-12-03 21:10:53 -0600

    Fixed test maker

--- project/cache/cache_direct/verification/trace_to_test.sh
+++ project/cache/cache_direct/verification/trace_to_test.sh
@@ -1,7 +1,7 @@
-cut -c 53-80 ../verification/trace.txt > ../verification/inter.txt
+cut -c 53-67,90-95 ../verification/trace.txt > ../verification/inter.txt
 sed -i 's/Wr/1 0/g' ../verification/inter.txt
 sed -i 's/Rd/0 1/g' ../verification/inter.txt
 sed -i 's/Addr //g' ../verification/inter.txt
-sed -i 's/Value //g' ../verification/inter.txt
 sed -i '/^$/d' ../verification/inter.txt
 awk '{ printf "%s %s %d %d\n", $1, $2, strtonum($3), strtonum($4) }' ../verification/inter.txt > ../verification/test.addr
+rm ../verification/inter.txt
