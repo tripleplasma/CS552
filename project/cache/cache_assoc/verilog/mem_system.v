2c30f878cd00bb67322f301360df63ec72da9298
kmvu2 <khiemvu117@gmail.com>
2024-12-05 19:30:23 -0600

    initial 2-way set associative

--- project/cache/cache_assoc/verilog/mem_system.v
+++ project/cache/cache_assoc/verilog/mem_system.v
@@ -26,7 +26,7 @@ module mem_system(/*AUTOARG*/
 
    // cache controller signals
    // cache inputs
-   reg cache_en;
+   reg cache_en_0, cache_en_1;
    reg cache_comp, cache_write;
    reg [15:0] cache_data_in, cache_addr;
 
@@ -38,6 +38,7 @@ module mem_system(/*AUTOARG*/
    wire [4:0] actual_tag_1;
    wire [15:0] cache_data_out_0;
    wire [15:0] cache_data_out_1;
+   wire [15:0] cache_data_out;
 
    // 4-bank memory inputs
    reg mem_write, mem_read;
@@ -53,15 +54,6 @@ module mem_system(/*AUTOARG*/
 
    reg [2:0] cache_offset;
 
-   //TODO: Don't invert on these conditions
-   // - instructions that do not read or write cache
-   // - invalid instructions
-   // - instructions that are squashed due to branch misprediction (not for cache demo, worry about for integrating with pipeline)
-   wire victimway, victimway_ff, 
-   reg toggle_victimway;
-   assign victimway = (toggle_victimway) ? ~victimway_ff : victimway_ff;
-   dff victimway_ff(.d(victimway), .q(victimway_ff), .clk(clk), .rst(rst));
-
    //NOTE: cache0 and cache1 are apart of the same SET, thus it is arbitrary which cache the data_in gets place into.
    // Because it doesn't matter (jk, they tell us which cache to send each addr to in the writeup) which cache we write to, 
    // we have to search both caches for the best one to evict
@@ -74,7 +66,7 @@ module mem_system(/*AUTOARG*/
                           .valid                (cache_valid_0),
                           .err                  (cache_err_0),
                           // Inputs
-                          .enable               (cache_en),
+                          .enable               (cache_en_0),
                           .clk                  (clk),
                           .rst                  (rst),
                           .createdump           (createdump),
@@ -95,7 +87,7 @@ module mem_system(/*AUTOARG*/
                           .valid                (cache_valid_1),
                           .err                  (cache_err_1),
                           // Inputs
-                          .enable               (cache_en & victimway_ff),
+                          .enable               (cache_en_1),
                           .clk                  (clk),
                           .rst                  (rst),
                           .createdump           (createdump),
@@ -153,30 +145,60 @@ module mem_system(/*AUTOARG*/
    dff valid1_ff (.d(cache_valid_1), .q(cache_valid_1_ff), .rst(rst), .clk(clk));
    dff dirty1_ff (.d(cache_dirty_1), .q(cache_dirty_1_ff), .rst(rst), .clk(clk));
 
+   wire [15:0] cache_data_out_ff;
+   assign cache_data_out = (cache_hit_1_ff & cache_valid_1_ff) ? cache_data_out_1 : 
+                           (cache_hit_0_ff & cache_valid_0_ff) ? cache_data_out_0 :
+                           cache_data_out_ff;
+   dff cache_data_out_dff[15:0](.d(cache_data_out), .q(cache_data_out_ff), .rst(rst), .clk(clk));
+
+   //TODO: Don't invert on these conditions
+   // - instructions that do not read or write cache
+   // - invalid instructions
+   // - instructions that are squashed due to branch misprediction (not for cache demo, worry about for integrating with pipeline)
+   wire victimway, victimway_ff;
+   reg toggle_victimway;
+   assign victimway = (toggle_victimway) ? ~victimway_ff : victimway_ff;
+   dff iVICTIMWAY_ff(.d(victimway), .q(victimway_ff), .clk(clk), .rst(rst));
+
+   wire real_hit_0, real_hit_1, real_hit;
+   reg victimize_cache_0, victimize_cache_1;
+
+   assign real_hit_0 = (cache_hit_0_ff & cache_valid_0_ff);
+   assign real_hit_1 = (cache_hit_1_ff & cache_valid_1_ff);
+   assign real_hit = real_hit_0 | real_hit_1;
+
+   wire writeback, writeback_0, writeback_1;
+   assign writeback_0 = cache_dirty_0_ff & cache_valid_0_ff;
+   assign writeback_1 = cache_dirty_1_ff & cache_valid_1_ff;
+   assign writeback = writeback_0 | writeback_1;
+
    always @(cache_state or Rd or Wr) begin
       // Set default values
       // cache controller signals
       
-      cache_en = 1'b0;
+      cache_en_0 = 1'b0;
+      cache_en_1 = 1'b0;
       cache_comp = 1'b0;
       cache_write = Wr;
       cache_data_in = DataIn;
       cache_addr = Addr;
       cache_offset = cache_addr[2:0];
 
+      victimize_cache_0 = victimize_cache_0;
+      victimize_cache_1 = victimize_cache_1;
+
       // Top outputs
       Done = 1'b0;
       toggle_victimway = 1'b0;
       Stall = (Rd | Wr) & ~Done;
-      DataOut = cache_data_out_0;
+      DataOut = cache_data_out;
       CacheHit = 1'b0;
       err = cache_err_0 | cache_err_1 | mem_err;
-   
    
       // 4-bank memory inputs
       mem_write = 1'b0;
       mem_read = 1'b0;
-      mem_data_in = cache_data_out_0;
+      mem_data_in = cache_data_out;
       mem_addr = cache_addr;
 
       // Default next_state to current state
@@ -186,7 +208,8 @@ module mem_system(/*AUTOARG*/
       case (cache_state)
          // IDLE
          5'b00000: begin
-            cache_en = (Rd | Wr);
+            cache_en_0 = (Rd | Wr);
+            cache_en_1 = (Rd | Wr);
             cache_comp = (Rd | Wr);
             nxt_state = (Rd | Wr) ? 5'b00010 : 5'b00000;
          end
@@ -194,27 +217,36 @@ module mem_system(/*AUTOARG*/
          // Read or write comparison
          5'b00001: begin
             // Access to see if hit
-            cache_en = 1'b1;
+            cache_en_0 = 1'b1;
+            cache_en_1 = 1'b1;
             cache_comp = 1'b1;
             nxt_state = 5'b00010;
          end
 
          // Check if Hit state
          5'b0010: begin
+            victimize_cache_0 = (~real_hit & 
+                                 (~cache_valid_0_ff | 
+                                 (cache_valid_0_ff & cache_valid_1_ff & ~victimway_ff))) ? 1'b1 : 1'b0;
+            victimize_cache_1 = (~real_hit & 
+                                 ((~cache_valid_1_ff & cache_valid_0_ff) | 
+                                 (cache_valid_0_ff & cache_valid_1_ff & victimway_ff))) ? 1'b1 : 1'b0;
             // Miss so need to do access read
-            cache_en = ~((cache_hit_0_ff & cache_valid_0_ff) | (cache_hit_1_ff & cache_valid_1_ff)) ? 1'b1 : cache_en;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : cache_en_0; // real_hit_0?
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : cache_en_1; // real_hit_1?
             //cache_comp = 1'b0 by default
 
-            cache_write = (~cache_hit_0_ff | ~cache_valid_0_ff) ? 1'b0 : cache_write;
-            cache_offset = (~cache_hit_0_ff | ~cache_valid_0_ff) ? 3'b000 : cache_offset;
+            cache_write = (~real_hit) ? 1'b0 : cache_write;
+            cache_offset = (~real_hit) ? 3'b000 : cache_offset;
 
             //TODO:  Use one of the hit outputs as a select for a mux between the two data outputs
             //Hit so done
-            Done = (cache_hit_0_ff & cache_valid_0_ff) | (cache_hit_1_ff & cache_valid_1_ff);
-            toggle_victimway = (cache_hit_0_ff & cache_valid_0_ff) | (cache_hit_1_ff & cache_valid_1_ff);
-            CacheHit = ((cache_hit_0_ff & cache_valid_0_ff) | (cache_hit_1_ff & cache_valid_1_ff));
+            Done = real_hit;
+            toggle_victimway = real_hit;
+            CacheHit = real_hit;
+            // cache_data_out = (cache_hit_1_ff & cache_valid_1_ff) ? cache_data_out_1 : cache_data_out_0;
 
-            nxt_state = (~cache_hit_0_ff | ~cache_valid_0_ff) ? 5'b00100 : ((Wr | Rd) ? 5'b00001 : 5'b00000);
+            nxt_state = (~real_hit) ? 5'b00100 : ((Wr | Rd) ? 5'b00001 : 5'b00000);
          end
 
          // Check if dirty state
@@ -223,46 +255,25 @@ module mem_system(/*AUTOARG*/
             //      If neither is valid, select way zero. If both are valid, use the pseudo-random replacement algorithm specified below.
 
             // Dirty so need to do writeback
-            cache_en = (cache_dirty_0_ff & cache_valid_0_ff) ? 1'b1 : cache_en;
+            cache_en_0 = (writeback_0 & victimize_cache_0) ? 1'b1 : cache_en_0;
+            cache_en_1 = (writeback_1 & victimize_cache_1) ? 1'b1 : cache_en_1;
             //cache_comp = 1'b0 by default
-            cache_write = (cache_dirty_0_ff & cache_valid_0_ff) ? 1'b0 : cache_write;
-            cache_offset = (cache_dirty_0_ff & cache_valid_0_ff) ? 3'b010 : cache_offset;
-            mem_write = (cache_dirty_0_ff & cache_valid_0_ff) ? 1'b1 : mem_write;
-            mem_addr = (cache_dirty_0_ff & cache_valid_0_ff) ? {actual_tag_0, cache_addr[10:3], 3'b000} : {cache_addr[15:3], 3'b000};
+            cache_write = (writeback) ? 1'b0 : cache_write;
+            cache_offset = (writeback) ? 3'b010 : cache_offset;
+            mem_write = (writeback) ? 1'b1 : mem_write;
+            mem_addr = (writeback_0 & victimize_cache_0) ? {actual_tag_0, cache_addr[10:3], 3'b000} : 
+                        (writeback_1 & victimize_cache_1) ? {actual_tag_1, cache_addr[10:3], 3'b000} :
+                                                            {cache_addr[15:3], 3'b000};
 
-            mem_read = ~(cache_dirty_0_ff & cache_valid_0_ff) ? 1'b1 : mem_read;
+            mem_read = ~(writeback) ? 1'b1 : mem_read;
 
-            nxt_state = (cache_dirty_0_ff & cache_valid_0_ff) ? 5'b01000 : 5'b00101;
-         end
-
-         // Mem read cycle 1
-         5'b00101: begin
-            //Read the second word for the cache line
-            mem_read = 1'b1;
-            mem_addr = {cache_addr[15:3], 3'b010};
-
-            nxt_state = 5'b00111;
-         end
-
-         // Mem read cycle 2
-         5'b00111: begin
-            //Read the third word for the cache line
-            mem_read = 1'b1;
-            mem_addr = {cache_addr[15:3], 3'b100};
-
-            cache_offset = 3'b000;
-            cache_en = 1'b1;
-            cache_comp = 1'b0;
-            cache_write = 1'b1;
-            cache_data_in = mem_data_out;
-
-            // Do access write to cache next
-            nxt_state = 5'b01100;
+            nxt_state = (writeback) ? 5'b01000 : 5'b00101;
          end
 
          // Mem write cycle 1
          5'b01000: begin
-            cache_en = 1'b1;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : 1'b0;
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : 1'b0;
             cache_comp = 1'b0;
             cache_write = 1'b0;
             cache_offset = 3'b100;
@@ -274,7 +285,8 @@ module mem_system(/*AUTOARG*/
 
          // Mem write cycle 2
          5'b01001: begin
-            cache_en = 1'b1;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : 1'b0;
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : 1'b0;
             cache_comp = 1'b0;
             cache_write = 1'b0;
             cache_offset = 3'b110;
@@ -298,6 +310,32 @@ module mem_system(/*AUTOARG*/
             nxt_state = 5'b00101;
          end
 
+         // Mem read cycle 1
+         5'b00101: begin
+            //Read the second word for the cache line
+            mem_read = 1'b1;
+            mem_addr = {cache_addr[15:3], 3'b010};
+
+            nxt_state = 5'b00111;
+         end
+
+         // Mem read cycle 2
+         5'b00111: begin
+            //Read the third word for the cache line
+            mem_read = 1'b1;
+            mem_addr = {cache_addr[15:3], 3'b100};
+
+            cache_offset = 3'b000;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : 1'b0;
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : 1'b0;
+            cache_comp = 1'b0;
+            cache_write = 1'b1;
+            cache_data_in = mem_data_out;
+
+            // Do access write to cache next
+            nxt_state = 5'b01100;
+         end
+
          // Access write to cache
          5'b01100: begin
             //Read the fourth word for the cache line
@@ -305,7 +343,8 @@ module mem_system(/*AUTOARG*/
             mem_addr = {cache_addr[15:3], 3'b110};
 
             cache_offset = 3'b010;
-            cache_en = 1'b1;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : 1'b0;
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : 1'b0;
             cache_comp = 1'b0;
             cache_write = 1'b1;
             cache_data_in = mem_data_out;
@@ -317,7 +356,8 @@ module mem_system(/*AUTOARG*/
          //TODO: Add 3 more states simply to write the values of the three other words into the cache
          5'b10000: begin
             cache_offset = 3'b100;
-            cache_en = 1'b1;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : 1'b0;
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : 1'b0;
             cache_comp = 1'b0;
             cache_write = 1'b1;
             cache_data_in = mem_data_out;
@@ -326,7 +366,8 @@ module mem_system(/*AUTOARG*/
 
          5'b10001: begin
             cache_offset = 3'b110;
-            cache_en = 1'b1;
+            cache_en_0 = (victimize_cache_0) ? 1'b1 : 1'b0;
+            cache_en_1 = (victimize_cache_1) ? 1'b1 : 1'b0;
             cache_comp = 1'b0;
             cache_write = 1'b1;
             cache_data_in = mem_data_out;
@@ -335,7 +376,8 @@ module mem_system(/*AUTOARG*/
 
          // Done with cache miss, do comp read or write
          5'b01110: begin
-            cache_en = 1'b1;
+            cache_en_0 = 1'b1;
+            cache_en_1 = 1'b1;
             cache_comp = 1'b1;
             // Assert done next
             nxt_state = 5'b01111;
@@ -347,7 +389,8 @@ module mem_system(/*AUTOARG*/
             Done = 1'b1;
             toggle_victimway = 1'b1;
 
-            cache_en = (Wr | Rd) ? 1'b1 : cache_en;
+            cache_en_0 = (Wr | Rd) ? 1'b1 : cache_en_0;
+            cache_en_1 = (Wr | Rd) ? 1'b1 : cache_en_1;
             cache_comp = (Wr | Rd) ? 1'b1 : cache_comp;
             
             nxt_state = (Wr | Rd) ? 5'b00010 : 5'b00000;
