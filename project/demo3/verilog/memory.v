fb7655e2345a7608f311c6170fe3daa626bc449d
tripleplasma <kalee9@wisc.edu>
2024-12-17 16:00:39 -0600

    Memory done

--- project/demo3/verilog/memory.v
+++ project/demo3/verilog/memory.v
@@ -6,49 +6,56 @@
                      processor.
 */
 `default_nettype none
-module memory (
-   input  wire          clk,
-   input  wire          rst,
-   input  wire          mem_en,
-   input  wire          write_en,
-   input  wire [15:0]   addr_in,
-   input  wire [15:0]   write_data_in,
-   output wire          stall_out,
-   output wire          done_out,
-   output wire          halt_mem,
-   output wire [15:0]   read_data
+module memory (clk, rst, mem_en,
+               write_en, addr_in,
+               write_data_in, stall_out, done_out, halt_mem, read_data
 );
+input  wire          clk;
+input  wire          rst;
+input  wire          mem_en;
+input  wire          write_en;
+input  wire [15:0]   addr_in;
+input  wire [15:0]   write_data_in;
+output wire          stall_out;
+output wire          done_out;
+output wire          halt_mem;
+output wire [15:0]   read_data;
 
+//Required downward paths
 wire done, stall, cache_hit;
-
 assign stall_out = stall;
 assign done_out = done;
 
-wire [15:0] addr_in_q, write_data_in_q;
-wire write_en_q, mem_en_q;
-dff write_en_ff (.clk(clk), .rst(rst), .d(stall_out ? write_en_q : write_en), .q(write_en_q));
-dff mem_en_ff (.clk(clk), .rst(rst), .d(stall_out ? mem_en_q : mem_en), .q(mem_en_q));
-dff addr_in_ff [15:0] (.clk(clk), .rst(rst), .d(stall_out ? addr_in_q : addr_in), .q(addr_in_q));
-dff write_data_in_ff [15:0] (.clk(clk), .rst(rst), .d(stall_out ? write_data_in_q : write_data_in), .q(write_data_in_q));
+wire [15:0] addr_in_delayed, write_data_in_delayed;
+wire write_en_delayed, mem_en_delayed;
 
-wire rd_set, wr_set;
-wire [15:0] addr_set, data_in_set;
+register #(.REGISTER_WIDTH(1)) iwriteEn(.clk(clk), .rst(rst), .writeEn(~stall_out), .writeData(write_en), .readData(write_en_delayed));
 
-assign rd_set = stall_out ? mem_en_q & ~write_en_q : mem_en & ~write_en;
-assign wr_set = stall_out ? mem_en_q & write_en_q : mem_en & write_en;
-assign addr_set = stall_out ? addr_in_q : addr_in;
-assign data_in_set = stall_out ? write_data_in_q : write_data_in;
+register #(.REGISTER_WIDTH(1)) imemEn(.clk(clk), .rst(rst), .writeEn(~stall_out), .writeData(mem_en), .readData(mem_en_delayed));
+
+register addr(.clk(clk), .rst(rst), .writeEn(~stall_out), .writeData(addr_in), .readData(addr_in_delayed));
+
+register writeData(.clk(clk), .rst(rst), .writeEn(~stall_out), .writeData(write_data_in), .readData(write_data_in_delayed));
+
+wire readMem, writeMem;
+wire [15:0] addr_mem, data_in_mem;
+
+assign readMem = stall_out ? mem_en_delayed & ~write_en_delayed : mem_en & ~write_en;
+assign writeMem = stall_out ? mem_en_delayed & write_en_delayed : mem_en & write_en;
+assign addr_mem = stall_out ? addr_in_delayed : addr_in;
+assign data_in_mem = stall_out ? write_data_in_delayed : write_data_in;
 
 mem_system #(1) data_mem(
    .clk        (clk),
    .rst        (rst),
    .err        (halt_mem),
-   .Rd         (rd_set),
-   .Wr         (wr_set),
-   .Addr       (addr_set),
-   .DataIn     (data_in_set),
-   .DataOut    (read_data), 
+   .Rd         (readMem),
+   .Wr         (writeMem),
+   .Addr       (addr_mem),
+   .DataIn     (data_in_mem), 
    .createdump (1'b0),
+   //Outputs
+   .DataOut    (read_data),
    .Done       (done),
    .Stall      (stall),
    .CacheHit   (cache_hit)   
