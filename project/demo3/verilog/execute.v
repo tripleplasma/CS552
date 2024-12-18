294e68c68ae0b9a7f010cbe9cd780196bc2ec246
kmvu2 <khiemvu117@gmail.com>
2024-12-17 18:46:00 -0600

    starting execute

--- project/demo3/verilog/execute.v
+++ project/demo3/verilog/execute.v
@@ -6,40 +6,40 @@
 */
 `default_nettype none
 module execute (
-   input  wire [15:0]   exec_out_fmem,
-   input  wire [15:0]   write_data,
-   input  wire [15:0]   instruction,
-   input  wire [15:0]   pc_in,
-   input  wire [15:0]   read_data1,
-   input  wire [15:0]   read_data2,
-   input  wire [15:0]   imm5_ext,
-   input  wire [15:0]   imm8_ext,
-   input  wire [15:0]   imm11_ext,
-   input  wire [2:0]    write_reg_ex_mem,
-   input  wire [2:0]    write_reg_mem_wb,
-   input  wire [2:0]    RegisterRs_id_ex,
-   input  wire [2:0]    RegisterRt_id_ex,
-   input  wire          write_en_ex_mem,
-   input  wire          write_en_mem_wb,
-   input  wire          ImmSrc,
-   input  wire [1:0]    BSrc,
-   input  wire [1:0]    Instr_Funct_out,     //  = Instruction[1:0]
-   input  wire [3:0]    ALUOp,
-   input  wire          ALUJump,
-   input  wire          InvB,
-   input  wire          InvA,
-   input  wire          Sub,
-   input  wire          Set,
-   input  wire          Branch,
-   input  wire          SLBI,
-   input  wire          BTR,
-   input  wire [1:0]    Instr_BrchCnd_sel, // = Instruction[12:11]
-   input  wire          jmp,
-   input  wire          stall_mem_stg,
-   output wire			PCSrc,
-   output wire [15:0]   exec_out,
-   output wire [15:0]   mem_addr_out,
-   output wire [15:0]   pc_jmp_out
+   input  wire [15:0]   aluOut_fwd,
+   input  wire [15:0]   writeData_wb,
+   input  wire [15:0]   instruction_e,
+   input  wire [15:0]   PC_e,
+   input  wire [15:0]   read1Data_e,
+   input  wire [15:0]   read2Data_e,
+   input  wire [15:0]   imm5Ext_e,
+   input  wire [15:0]   imm8Ext_e,
+   input  wire [15:0]   imm11Ext_e,
+   input  wire [2:0]    writeRegSel_m,
+   input  wire [2:0]    writeRegSel_wb,
+   input  wire [2:0]    regRs_e,
+   input  wire [2:0]    regRt_e,
+   input  wire          regWrite_m,
+   input  wire          regWrite_wb,
+   input  wire          immExtSel_e,
+   input  wire [1:0]    B_int_e,
+   input  wire [1:0]    extension_e,
+   input  wire [3:0]    aluOp_e,
+   input  wire          aluJmp_e,
+   input  wire          invA_e,
+   input  wire          invB_e,
+   input  wire          subtract_e,
+   input  wire          shift_e,
+   input  wire          branch_e,
+   input  wire          slbi_e,
+   input  wire          btr_e,
+   input  wire [1:0]    branchSel_e,
+   input  wire          jmp_e,
+   input  wire          dataMem_stall,
+   output wire			   PCSrc,
+   output wire [15:0]   aluOut_e,
+   output wire [15:0]   read2Data_em,
+   output wire [15:0]   PC_jmp_e
 );
 
    reg  [15:0] imm_rd2;
@@ -72,17 +72,17 @@ module execute (
 /////// Forwarding Unit ///////////////////
 
 // Check if operation is R type or a ST/STU
-assign r_type = ((instruction[15:14] == 2'b11) & (instruction[15:11] != 5'b11000)) | 
-                ((instruction[15:11] == 5'b10000) | (instruction[15:11] == 5'b10011));
+assign r_type = ((instruction_e[15:11] == 5'b10000) | (instruction_e[15:11] == 5'b10011)) | 
+               ((instruction_e[15:14] == 2'b11) & (instruction_e[15:11] != 5'b11000));
 
 forwarding forwarding_unit (
    .r_type              (r_type),
-   .write_reg_ex_mem    (write_reg_ex_mem),
-   .write_reg_mem_wb    (write_reg_mem_wb),
-   .RegisterRs_id_ex    (RegisterRs_id_ex),
-   .RegisterRt_id_ex    (RegisterRt_id_ex),
-   .write_en_ex_mem     (write_en_ex_mem),
-   .write_en_mem_wb     (write_en_mem_wb),
+   .write_reg_ex_mem    (writeRegSel_m),
+   .write_reg_mem_wb    (writeRegSel_wb),
+   .RegisterRs_id_ex    (regRs_e),
+   .RegisterRt_id_ex    (regRt_e),
+   .write_en_ex_mem     (regWrite_m),
+   .write_en_mem_wb     (regWrite_wb),
    .ForwardA            (ForwardA),
    .ForwardB            (ForwardB)
 );
@@ -90,30 +90,32 @@ forwarding forwarding_unit (
 
 /////// A & B INPUT MUXES /////////////////
 
-assign BaseA = (ForwardA == 2'b10) ? exec_out_fmem :
-               (ForwardA == 2'b01) ? write_data    : read_data1;
+assign BaseA = (ForwardA == 2'b10) ? aluOut_fwd :
+               (ForwardA == 2'b01) ? writeData_wb : 
+               read1Data_e;
 
-assign BaseB = (ForwardB == 2'b10) ? exec_out_fmem :
-               (ForwardB == 2'b01) ? write_data    : read_data2;
+assign BaseB = (ForwardB == 2'b10) ? aluOut_fwd :
+               (ForwardB == 2'b01) ? writeData_wb : 
+               read2Data_e;
 
-assign mem_addr_out = BaseB;
+assign read2Data_em = BaseB;
 
 // A signal mux
-assign ALU_inA = SLBI ? BaseA << 8 : BaseA;
+assign ALU_inA = slbi_e ? BaseA << 8 : BaseA;
 assign ALU_inB = brch_zero;
 
 //  mux for B signal w/ imms and read data 2
 always @(*) begin
-   case(BSrc)
+   case(B_int_e)
       2'b00: imm_rd2 = BaseB;
-      2'b01: imm_rd2 = imm5_ext;
-      2'b10: imm_rd2 = imm8_ext;
-      2'b11: imm_rd2 = imm11_ext;
+      2'b01: imm_rd2 = imm5Ext_e;
+      2'b10: imm_rd2 = imm8Ext_e;
+      2'b11: imm_rd2 = imm11Ext_e;
    endcase
 end
 
 // branch instr mux for B signal
-assign brch_zero = Branch ? 16'h0000 : imm_rd2;
+assign brch_zero = branch_e ? 16'h0000 : imm_rd2;
 
 ////////////////////////////////////////////
 
@@ -122,18 +124,18 @@ assign brch_zero = Branch ? 16'h0000 : imm_rd2;
 //// BRANCHING MUXES //////////////////////////////////////////////////
 
 // imm src mux for next instr
-assign imm_mux = ImmSrc ? imm11_ext : imm8_ext;
+assign imm_mux = immExtSel_e ? imm11Ext_e : imm8Ext_e;
 
 // add imm to pc
-fulladder16 iFA (.A(imm_mux), .B(pc_in), .Cin(1'b0), .S(imm_plus_pc), .Cout());
+fulladder16 iFA (.A(imm_mux), .B(PC_e), .Cin(1'b0), .S(imm_plus_pc), .Cout());
 
 // choose between current pc or calculated branch instr
-assign brch_mux = (BRJmp | jmp) ? imm_plus_pc : pc_in;
+assign brch_mux = (BRJmp | jmp_e) ? imm_plus_pc : PC_e;
 
 // choose if we'll also be taking next instr addr from ALU
-assign pc_jmp_out = ALUJump ? alu_out : brch_mux;
+assign PC_jmp_e = aluJmp_e ? alu_out : brch_mux;
 
-assign PCSrc = (~stall_mem_stg) ? ALUJump | BRJmp | jmp : 1'b0;
+assign PCSrc = (~dataMem_stall) ? aluJmp_e | BRJmp | jmp_e : 1'b0;
 
 //////////////////////////////////////////////////////////////////////
 
@@ -141,28 +143,28 @@ assign PCSrc = (~stall_mem_stg) ? ALUJump | BRJmp | jmp : 1'b0;
 
 // choose ALU oper
 // checking for an R format subtraction op and setting OP bits to 3'b100 if true
-// for R format instr (exclduing sub) set last 2 bits to op code ext.
-// else use 3 upper ALUOp bits
-assign ALUOp_mux = ALUOp[0] ? ((Instr_Funct_out == 2'b01) & ALUOp[3] ? 3'b100 : {ALUOp[3], Instr_Funct_out}) : ALUOp[3:1];
+// for R format instr (exclduing subtract_e) set last 2 bits to op code ext.
+// else use 3 upper aluOp_e bits
+assign ALUOp_mux = aluOp_e[0] ? ((extension_e == 2'b01) & aluOp_e[3] ? 3'b100 : {aluOp_e[3], extension_e}) : aluOp_e[3:1];
 
-// set InvA for R format SUB
-assign InvA_ALU = ALUOp[0] & (Instr_Funct_out == 2'b01) & ALUOp[3] ? 1'b1 : InvA;
+// set invA_e for R format SUB
+assign InvA_ALU = aluOp_e[0] & (extension_e == 2'b01) & aluOp_e[3] ? 1'b1 : invA_e;
 
-// set InvB for R format ANDN
-assign InvB_ALU = ALUOp[0] & (Instr_Funct_out == 2'b11) & ALUOp[3] ? 1'b1 : InvB;
+// set invB_e for R format ANDN
+assign InvB_ALU = aluOp_e[0] & (extension_e == 2'b11) & aluOp_e[3] ? 1'b1 : invB_e;
 
 // set Sub for R format SUB
-assign Sub_ALU = ALUOp[0] & (Instr_Funct_out == 2'b01) & ALUOp[3] ? 1'b1 : Sub;
+assign Sub_ALU = aluOp_e[0] & (extension_e == 2'b01) & aluOp_e[3] ? 1'b1 : subtract_e;
 
 // instantiate ALU & set flags
 alu iALU (.InA(ALU_inA), .InB(ALU_inB), .Cin(Sub_ALU), .Oper(ALUOp_mux), .invA(InvA_ALU), .invB(InvB_ALU), .Out(alu_out), .Zero(ZF), .Ofl(OF), .Cout(CF));
 assign SF = alu_out[15];
 
 // BTR instr mux
-assign btr_mux = BTR ? {BaseA[0], BaseA[1], BaseA[2], BaseA[3], BaseA[4], BaseA[5], BaseA[6], BaseA[7], BaseA[8], BaseA[9], BaseA[10], BaseA[11], BaseA[12], BaseA[13], BaseA[14], BaseA[15]} : alu_out;
+assign btr_mux = btr_e ? {BaseA[0], BaseA[1], BaseA[2], BaseA[3], BaseA[4], BaseA[5], BaseA[6], BaseA[7], BaseA[8], BaseA[9], BaseA[10], BaseA[11], BaseA[12], BaseA[13], BaseA[14], BaseA[15]} : alu_out;
 
 // output mux
-assign exec_out = Set ? set_out : btr_mux;
+assign aluOut_e = shift_e ? set_out : btr_mux;
 
 /////////////////////////////////////////////////////////////////////
 
@@ -172,18 +174,18 @@ assign exec_out = Set ? set_out : btr_mux;
 
 // branching logic (take bits [12:11] of instr)
 always @(*) begin
-   case (Instr_BrchCnd_sel)
+   case (branchSel_e)
       2'b00 : BRJmp_mux = ZF; //BEQZ
       2'b01 : BRJmp_mux = ~ZF; //BNEZ
       2'b10 : BRJmp_mux = SF; //BLTZ
       2'b11 : BRJmp_mux = ~SF | ZF; //BGEZ
    endcase
 end
-assign BRJmp = BRJmp_mux & Branch;
+assign BRJmp = BRJmp_mux & branch_e;
 
 // set instr logic
 always @(*) begin
-   case (Instr_BrchCnd_sel)
+   case (branchSel_e)
       2'b00 : set_out = ZF ? 16'h0001 : 16'h0000; // SEQ (Rs^Rt)
       2'b01 : set_out = (ALU_inA[15] & ~ALU_inB[15]) | (SF & ~OF) ? 16'h0001 : 16'h0000; // SLT (Rs-Rt)
       2'b10 : set_out = (ALU_inA[15] & ~ALU_inB[15]) | ((SF | ZF) & ~OF) ? 16'h0001 : 16'h0000; // SLE (Rs-Rt)
